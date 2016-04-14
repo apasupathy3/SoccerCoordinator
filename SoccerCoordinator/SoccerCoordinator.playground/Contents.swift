5acd58824cd776f88568a8b9888a188788ab1c37
Anjali Pasupathy <Anjali@muthus-mac-mini.local>
2016-04-14 10:12:42 -0500

    1: Renamed "Courses this project covers" to "TOOLBOX" 2: Updated buildSoccerPlayer to create [String:String] rather than [String:AnyObject] 4: Updated description for Soccer Players Model to reflect change to buildSoccerPlayer 5: Updated MORE ADVANCED TOOLS for Soccer Players Model to incorporate [String:AnyObject] 6: Updated soccerPlayers Array to be Array of [String:String], not [String:AnyObject] 7: Uncapitalized team Arrays 8: Updated type for sorted players Arrays 9: Provided flimsy explanation for use of Optionals and if-let statements     Created MORE ADVANCED TOOLS section for sort loop 10-13: Created new, more cumbersome methods for sorting players by experience and height because there are very few tools I have to work with
    NEXT STEP: MAKE THAT CUMBERSOME CODE MORE DRY!

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /*
- Courses this project covers:
+ TOOLBOX:
     Swift 2.0 Basics
     Swift 2.0 Collections and Control Flow
     Swift 2.0 Functions
@@ -16,12 +16,12 @@ import Foundation
 /*
  Parameters: values required to build a Soccer Player element
  playerName: The name of the soccer player as a String
- heightInInches: The height of the soccer player in inches as an Int
- hasSoccerExperience: Bool indicating whether or not player has previous soccer experience
- guardianNames: Array of the name(s) of the player's guardian(s) as String(s)
- Returns Soccer Player as NSDictionary in format specified by Model for Soccer Players below
+ heightInInches: The height of the soccer player in inches as the String of an Int
+ hasSoccerExperience: String "YES" or "NO" indicating whether or not player has previous soccer experience
+ guardianNames: The name(s) of the player's guardian(s) as a String
+ Returns Soccer Player as [String:String] in format specified by Model for Soccer Players below
  */
-func buildSoccerPlayer(playerName: String, heightInInches: Int, hasSoccerExperience: Bool, guardianNames: [String]) -> [String:AnyObject] {
+func buildSoccerPlayer(playerName: String, heightInInches: String, hasSoccerExperience: String, guardianNames: String) -> [String:String] {
     return [
         "playerName": playerName,
         "heightInInches": heightInInches,
@@ -33,36 +33,43 @@ func buildSoccerPlayer(playerName: String, heightInInches: Int, hasSoccerExperie
 /*
  Model for Soccer Players: Array of Dictionaries
     - Each element of array represents a soccer player
-    - Each soccer player contains the following keys and value-types:
-        "playerName": String
-        "heightInInches": Int
-        "hasSoccerExperience": Bool
-        "guardianNames": [String]
+    - Each soccer player contains the following keys: "playerName", "heightInInches", "hasSoccerExperience", and "guardianNames"
 */
 /*
  MORE ADVANCED TOOLS:
-    - Instead of using NSDictionary, creating a struct for SoccerPlayer with the properties being the keys for the NSDictionary model would be better
-    - Rather than typing in all the data from SoccerPlayerInfo, it would be better to read the file into the playground, extract the data from that file, and building a SoccerPlayer array from that data
+    - Rather than using [String:String], it would be better to use [String:AnyObject] so 
+        that the properties can hold values of the following types:
+            "playerName": String
+            "heightInInches": Int
+            "hasSoccerExperience": Bool
+            "guardianNames": String
+        * Using [String:AnyObject] would require casting AnyObject to the proper value and
+            unwrapping optionals, which isn't in the Toolbox available to me for this project
+    - Instead of using a Dictionary type, creating a struct for SoccerPlayer with the 
+        properties being the keys in the Dictionary would be better
+    - Rather than typing in all the data from SoccerPlayerInfo, it would be better to read 
+        the file into the playground, extract the data from that file, and build a Soccer
+        Player Array from that data
 */
 var soccerPlayers = [
-    buildSoccerPlayer("Joe Smith", heightInInches: 42, hasSoccerExperience: true, guardianNames: ["Jim Smith", "Jan Smith"]),
-    buildSoccerPlayer("Jill Tanner", heightInInches: 36, hasSoccerExperience: true, guardianNames: ["Clara Tanner"]),
-    buildSoccerPlayer("Bill Bon", heightInInches: 43, hasSoccerExperience: true, guardianNames: ["Sara Bon", "Jenny Bon"]),
-    buildSoccerPlayer("Eva Gordon", heightInInches: 45, hasSoccerExperience: false, guardianNames: ["Wendy Gordon", "Mike Gordon"]),
-    buildSoccerPlayer("Matt Gill", heightInInches: 40, hasSoccerExperience: false, guardianNames: ["Charles Gill", "Sylvia Gill"]),
-    buildSoccerPlayer("Kimmy Stein", heightInInches: 41, hasSoccerExperience: false, guardianNames: ["Bill Stein", "Hillary Stein"]),
-    buildSoccerPlayer("Sammy Adams", heightInInches: 45, hasSoccerExperience: false, guardianNames: ["Jeff Adams"]),
-    buildSoccerPlayer("Karl Saygan", heightInInches: 42, hasSoccerExperience: true, guardianNames: ["Heather Bledsoe"]),
-    buildSoccerPlayer("Suzane Greenberg", heightInInches: 44, hasSoccerExperience: true, guardianNames: ["Henrietta Dumas"]),
-    buildSoccerPlayer("Sal Dali", heightInInches: 41, hasSoccerExperience: false, guardianNames: ["Gala Dali"]),
-    buildSoccerPlayer("Joe Kavalier", heightInInches: 39, hasSoccerExperience: false, guardianNames: ["Sam Kavalier", "Elaine Kavalier"]),
-    buildSoccerPlayer("Ben Finkelstein", heightInInches: 44, hasSoccerExperience: false, guardianNames: ["Aaron Finkelstein", "Jill Finkelstein"]),
-    buildSoccerPlayer("Diego Soto", heightInInches: 41, hasSoccerExperience: true, guardianNames: ["Robin Soto", "Sarika Soto"]),
-    buildSoccerPlayer("Chloe Alaska", heightInInches: 47, hasSoccerExperience: false, guardianNames: ["David Alaska", "Jamie Alaska"]),
-    buildSoccerPlayer("Arnold Willis", heightInInches: 43, hasSoccerExperience: false, guardianNames: ["Claire Willis"]),
-    buildSoccerPlayer("Phillip Helm", heightInInches: 44, hasSoccerExperience: true, guardianNames: ["Thomas Helm", "Eva Jones"]),
-    buildSoccerPlayer("Les Clay", heightInInches: 42, hasSoccerExperience: true, guardianNames: ["Wynonna Brown"]),
-    buildSoccerPlayer("Herschel Krustofski", heightInInches: 45, hasSoccerExperience: true, guardianNames: ["Hyman Krustofski", "Rachel Krustofski"])
+    buildSoccerPlayer("Joe Smith", heightInInches: "42", hasSoccerExperience: "YES", guardianNames: "Jim and Jan Smith"),
+    buildSoccerPlayer("Jill Tanner", heightInInches: "36", hasSoccerExperience: "YES", guardianNames: "Clara Tanner"),
+    buildSoccerPlayer("Bill Bon", heightInInches: "43", hasSoccerExperience: "YES", guardianNames: "Sara and Jenny Bon"),
+    buildSoccerPlayer("Eva Gordon", heightInInches: "45", hasSoccerExperience: "NO", guardianNames: "Wendy and Mike Gordon"),
+    buildSoccerPlayer("Matt Gill", heightInInches: "40", hasSoccerExperience: "NO", guardianNames: "Charles and Sylvia Gill"),
+    buildSoccerPlayer("Kimmy Stein", heightInInches: "41", hasSoccerExperience: "NO", guardianNames: "Bill and Hillary Stein"),
+    buildSoccerPlayer("Sammy Adams", heightInInches: "45", hasSoccerExperience: "NO", guardianNames: "Jeff Adams"),
+    buildSoccerPlayer("Karl Saygan", heightInInches: "42", hasSoccerExperience: "YES", guardianNames: "Heather Bledsoe"),
+    buildSoccerPlayer("Suzane Greenberg", heightInInches: "44", hasSoccerExperience: "YES", guardianNames: "Henrietta Dumas"),
+    buildSoccerPlayer("Sal Dali", heightInInches: "41", hasSoccerExperience: "NO", guardianNames: "Gala Dali"),
+    buildSoccerPlayer("Joe Kavalier", heightInInches: "39", hasSoccerExperience: "NO", guardianNames: "Sam and Elaine Kavalier"),
+    buildSoccerPlayer("Ben Finkelstein", heightInInches: "44", hasSoccerExperience: "NO", guardianNames: "Aaron and Jill Finkelstein"),
+    buildSoccerPlayer("Diego Soto", heightInInches: "41", hasSoccerExperience: "YES", guardianNames: "Robin and Sarika Soto"),
+    buildSoccerPlayer("Chloe Alaska", heightInInches: "47", hasSoccerExperience: "NO", guardianNames: "David and Jamie Alaska"),
+    buildSoccerPlayer("Arnold Willis", heightInInches: "43", hasSoccerExperience: "NO", guardianNames: "Claire Willis"),
+    buildSoccerPlayer("Phillip Helm", heightInInches: "44", hasSoccerExperience: "YES", guardianNames: "Thomas Helm and Eva Jones"),
+    buildSoccerPlayer("Les Clay", heightInInches: "42", hasSoccerExperience: "YES", guardianNames: "Wynonna Brown"),
+    buildSoccerPlayer("Herschel Krustofski", heightInInches: "45", hasSoccerExperience: "YES", guardianNames: "Hyman and Rachel Krustofski")
 ]
 
 //--------------------------------------------------
@@ -77,31 +84,74 @@ var soccerPlayers = [
 */
 
 // Each team is an array of Soccer Players from the Soccer Players Model
-var Sharks = []
-var Dragons = []
-var Raptors = []
+var sharks = []
+var dragons = []
+var raptors = []
 
 // Before creating teams, split Soccer Players into experienced and unexperienced arrays
 // Use Quick Sort to sort arrays by Soccer Player heightInInches in ascending order
-var sortedInexperiencedPlayers: [[String:AnyObject]] = []
-var sortedExperiencedPlayers:[[String:AnyObject]] = []
+var sortedInexperiencedPlayers: [[String:String]] = []
+var sortedExperiencedPlayers:[[String:String]] = []
 
-// FIXME: CAN'T USE AS KEYWORD! CAN'T USE OPTIONAL FORCE UNWRAP!
+/*
+ NOTE:
+ In Swift Collection Types documentation (referenced by Swift 2.0 Collections and Control Flow),
+ Optional types are briefly mentioned and if-let statements are used in the examples.
+ Because of this, I'm going to assume that I can use them, despite the fact that Optional types
+ and if-let statements are officially introduced in a later lesson.
+*/
+/*
+ MORE ADVANCED TOOLS:
+    - Given we're using a [String:String] type for soccerPlayer, the as keyword would be
+        useful for shortening up those stacked if-lets
+    - Using a struct instead of a Dictionary would be even more useful because we wouldn't
+        have to deal with unwrapping Optionals of values we know for sure exist
+*/
 for soccerPlayer in soccerPlayers {
-    let heightKey = "heightInInches"
-    if soccerPlayer["hasSoccerExperience"] as! Bool {
+    if soccerPlayer["hasSoccerExperience"] == "YES" {
+        var insertPlayer = false
         var index = 0
-        while index < sortedExperiencedPlayers.count && (soccerPlayer[heightKey] as! Int) > (sortedExperiencedPlayers[index][heightKey] as! Int) {
-            index += 1
-        }
+        repeat {
+            if sortedExperiencedPlayers.isEmpty {
+                insertPlayer = true
+            } else {
+                if let experiencedPlayerHeight = sortedExperiencedPlayers[index]["heightInInches"],
+                   let experiencedPlayerHeightInt = Int(experiencedPlayerHeight),
+                   let soccerPlayerHeight = soccerPlayer["heightInInches"],
+                   let soccerPlayerHeightInt = Int(soccerPlayerHeight) {
+                    if soccerPlayerHeightInt <= experiencedPlayerHeightInt {
+                        insertPlayer = true
+                    } else {
+                        index += 1
+                    }
+                }
+            }
+        } while index < sortedExperiencedPlayers.count && !insertPlayer
         sortedExperiencedPlayers.insert(soccerPlayer, atIndex: index)
     } else {
+        var insertPlayer = false
         var index = 0
-        while index < sortedInexperiencedPlayers.count && (soccerPlayer[heightKey] as! Int) > (sortedInexperiencedPlayers[index][heightKey] as! Int) {
-            index += 1
-        }
+        repeat {
+            if sortedInexperiencedPlayers.isEmpty {
+                insertPlayer = true
+            } else {
+                if let inexperiencedPlayerHeight = sortedInexperiencedPlayers[index]["heightInInches"],
+                    let inexperiencedPlayerHeightInt = Int(inexperiencedPlayerHeight),
+                    let soccerPlayerHeight = soccerPlayer["heightInInches"],
+                    let soccerPlayerHeightInt = Int(soccerPlayerHeight) {
+                    if soccerPlayerHeightInt <= inexperiencedPlayerHeightInt {
+                        insertPlayer = true
+                    } else {
+                        index += 1
+                    }
+                }
+            }
+        } while index < sortedInexperiencedPlayers.count && !insertPlayer
         sortedInexperiencedPlayers.insert(soccerPlayer, atIndex: index)
     }
 }
+
+
+
 
 
